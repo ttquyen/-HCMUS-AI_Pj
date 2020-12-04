d4035f78498c1383ac48f678db2834003798e4c7
Thuy-Quyen <ttquyen18@apcs.vn>
2020-12-04 16:02:46 +0700

    Add check&move Functions
    Move agents 8 directions

--- map/buildMap.py
+++ map/buildMap.py
@@ -10,11 +10,7 @@ pygame.display.set_caption('hide & seek')
 HIDER = pygame.image.load('virus.png').convert()
 pygame.display.set_icon(HIDER)
 SEEKER = pygame.image.load('mask.png').convert()
-""""
-#init positions of SEEKER and HIDER
-SEEKER_INIT = (2,2)     #human
-HIDER_INIT = (23,23)    #virus
-"""
+
 GREY = pygame.Color(158,158,158)
 YELLOW = pygame.Color(255, 193, 7)
 BLACK = pygame.Color(0, 0, 0)
@@ -40,63 +36,65 @@ def loadMapToArr(path):
       row = []
 
 def setupGame(path):
+  #init positions of SEEKER and HIDER
+  Seek_xCoor = 10
+  Seek_yCoor = 5
+  Hider_xCoor = 23
+  Hider_yCoor = 23
   drawMap(path)
   mapString = getMapString(path)
   mapSize = getMapSize(path)
   squareSize = int(DISPLAY_SIZE / mapSize)
   loadMapToArr(path)
-  print(map)
-  #Posion Seeker:
-  #Seek_xCoor = 1*squareSize #SEEKER_INIT[0]
-  #Seek_yCoor = 1*squareSize #SEEKER_INIT[1]
-  #Position Hider:
-  #Hide_xCoor = 23*squareSize #HIDER_INIT[0]
-  #Hide_yCoor = 23*squareSize #HIDER_INIT[1]
-  Seek_xCoor = 1
-  Seek_yCoor = 1
-  Hider_xCoor = 23
-  Hider_yCoor = 23
+   #Draw Seeker:
+  COLORS['3'].set_colorkey
+  screen.blit(COLORS['3'], (Seek_xCoor*24, Seek_yCoor*24))
+  #Draw Hider:
+  COLORS['2'].set_colorkey
+  screen.blit(COLORS['2'], (Hider_xCoor*24, Hider_yCoor*24))
   RUNNING = True
   while RUNNING:
-
-    #Draw Seeker:
-    COLORS['3'].set_colorkey
-    screen.blit(COLORS['3'], (Seek_xCoor*24, Seek_yCoor*24))
-    #Draw Hider:
-    COLORS['2'].set_colorkey
-    screen.blit(COLORS['2'], (Hider_xCoor*24, Hider_yCoor*24))
-
-
     #Seek run to...: Algorithm here....
     time.sleep(0.3)
-    if(ableToMoveRight(Seek_xCoor, Seek_yCoor)==True):
-      Seek_xCoor +=1
-    elif(ableToMoveDown(Seek_xCoor, Seek_yCoor)==True):
-      Seek_yCoor +=1
-
-
-    ##Hide_xCoor +=squareSize
+ #**********Test some move functions******Chay dung a mn*************   
+  #  if(moveDown(Seek_xCoor, Seek_yCoor, 3)):
+  #    Seek_yCoor+=1
+  #  if(moveUp(Seek_xCoor, Seek_yCoor, 3)):
+  #    Seek_yCoor-=1
+  #  if(moveDownLeft(Seek_xCoor, Seek_yCoor,3)):
+  #    Seek_yCoor+=1
+  #    Seek_xCoor-=1
+  #  if(moveDownRight(Seek_xCoor, Seek_yCoor, 3)):
+  #    Seek_yCoor+=1
+  #    Seek_xCoor+=1
+  #  if(moveUpLeft(Seek_xCoor, Seek_yCoor, 3)):
+  #    Seek_yCoor-=1
+  #    Seek_xCoor-=1
+  #  if(moveUpRight(Seek_xCoor, Seek_yCoor, 3)):
+  #    Seek_yCoor-=1
+  #    Seek_xCoor+=1
+    if(moveLeft(Seek_xCoor, Seek_yCoor, 3)):
+      Seek_xCoor-=1
+  #  if(moveRight(Seek_xCoor, Seek_yCoor, 3)):
+  #    Seek_xCoor+=1
+     
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         RUNNING = False
     pygame.display.update()
-
-
 def getMapString(path):
   f = open(path, 'r')
   next(f)       ##bo dong dau tien
   mapString = f.read()
   f.close()
   return mapString
-
 def getMapSize(path):
   f = open(path, 'r')
   firstLine = f.readline()        
   size = int(firstLine[0] + firstLine[1])
   f.close()
   return size
-
 def drawMap(path):
   global DISPLAY_SIZE
   mapString = getMapString(path)
@@ -114,44 +112,105 @@ def drawMap(path):
       yCoor += squareSize 
       xCoor = 0
       count = 0
-
-
+def drawObjectAfterMove(curX, curY, nextX, nextY, typeOfAgent):
+  if(typeOfAgent == 3):
+    COLORS['3'].set_colorkey
+    screen.blit(COLORS['3'], (nextX*24, nextY*24))
+  if(typeOfAgent == 2):
+    COLORS['2'].set_colorkey
+    screen.blit(COLORS['2'], (nextX*24, nextY*24))
+  pygame.draw.rect(screen, COLORS['0'], [curX*24, curY*24, 24, 24]) #squareSize=24
+  
+#*******************check able to move ******************************#
 def ableToMoveRight(coorX, coorY):
-  if((map[coorX+1][coorY]==1) or (map[coorX+1][coorY]==4)):
+  if((map[coorY][coorX+1]!=0)):
     return False
   return True
-
 def ableToMoveLeft(coorX, coorY):
-  if((map[coorX-1][coorY]==1) or (map[coorX-1][coorY]==4)):
+  if((map[coorY][coorX-1]!=0)):
     return False
   return True
-
 def ableToMoveDown(coorX, coorY):
-  if((map[coorX][coorY+1]==1) or (map[coorX][coorY+1]==4)):
+  if((map[coorY+1][coorX]!=0)):
     return False
   return True
-
 def ableToMoveUp(coorX, coorY):
-  if((map[coorX][coorY-1]==1) or (map[coorX][coorY-1]==4)):
+  if((map[coorY-1][coorX]!=0)):
     return False
   return True
-
 def ableToMoveUpRight(coorX, coorY):
-  if((map[coorX+1][coorY-1]==1) or (map[coorX+1][coorY-1]==4)):
+  if((map[coorY-1][coorX+1]!=0)):
     return False
   return True
 def ableToMoveUpLeft(coorX, coorY):
-  if((map[coorX-1][coorY-1]==1) or (map[coorX-1][coorY-1]==4)):
+  if((map[coorY-1][coorX-1]!=0)):
     return False
   return True
-
 def ableToMoveDownLeft(coorX, coorY):
-  if((map[coorX-1][coorY+1]==1) or (map[coorX-1][coorY+1]==4)):
+  if((map[coorY+1][coorX-1]!=0)):
+    return False
+  return True
+def ableToMoveDownRight(coorX, coorY):
+  if(map[coorY+1][coorX+1]!=0):
     return False
   return True
 
-def ableToMoveDownRight(coorX, coorY):
-  if((map[coorX+1][coorY+1]==1) or (map[coorX+1][coorY+1]==4)):
-    return False
-  return True
-setupGame('map11.txt')
+#***************************move**************************#
+def moveRight(coorX, coorY, typeOfAgent):
+  if(ableToMoveRight(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY][coorX+1]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX+1,coorY,typeOfAgent)
+      return True
+  return False
+def moveLeft(coorX, coorY, typeOfAgent):
+  if(ableToMoveLeft(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY][coorX-1]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX-1,coorY,typeOfAgent)
+      return True
+  return False
+def moveUp(coorX, coorY, typeOfAgent):
+  if(ableToMoveUp(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY-1][coorX]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX,coorY-1,typeOfAgent)
+      return True
+  return False
+def moveDown(coorX, coorY, typeOfAgent):
+  if(ableToMoveDown(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY+1][coorX]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX,coorY+1,typeOfAgent)
+      return True
+  return False
+def moveDownLeft(coorX, coorY, typeOfAgent):
+  if(ableToMoveDownLeft(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY+1][coorX-1]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX-1,coorY+1,typeOfAgent)
+      return True
+  return False
+def moveDownRight(coorX, coorY, typeOfAgent):
+  if(ableToMoveDownRight(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY+1][coorX+1]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX+1,coorY+1,typeOfAgent)
+      return True
+  return False
+def moveUpRight(coorX, coorY, typeOfAgent):
+  if(ableToMoveUpRight(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY-1][coorX+1]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX+1,coorY-1,typeOfAgent)
+      return True
+  return False
+def moveUpLeft(coorX, coorY, typeOfAgent):
+  if(ableToMoveUpLeft(coorX, coorY)==True):
+      map[coorY][coorX]=0
+      map[coorY-1][coorX-1]=typeOfAgent
+      drawObjectAfterMove(coorX, coorY, coorX-1,coorY-1,typeOfAgent)
+      return True
+  return False
+
+setupGame('map14.txt')
